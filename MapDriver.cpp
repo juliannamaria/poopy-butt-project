93e4c19833f18dce1a04a824016f14a2058f710d
juliannamaria <167822171+juliannamaria@users.noreply.github.com>
2024-04-23 19:31:47 -0600

    Add files via upload

---
+++ MapDriver.cpp
@@ -0,0 +1,58 @@
+#include "Map.h"
+#include <ctype.h>
+#include <fstream>
+#include "Game.h"
+
+#define RED "\033[41m" 
+#define GREEN "\033[42m"
+#define BLUE "\033[44m"
+#define MAGENTA "\033[45m"
+#define CYAN "\033[46m"
+#define RESET "\033[0m"
+
+using namespace std;
+
+int main()
+{
+    /*CALL PLAY GAME FUNCTION HERE*/
+
+    //Print map
+    //Initialize random seed
+    srand (time(NULL));
+
+    Map starterMap = Map(); 
+    
+    //Initialize tiles
+    starterMap.initializeMap();
+
+    starterMap.displayMap();
+
+    Game game; 
+
+    game.playGame();
+
+    // //Demonstrate tiles
+    // //Top lane corresponds to Player 1
+    // for (int i = 0; i < 50; i++)
+    // {
+    //     cout << starterMap.getMapPos(0, i) << "1" << " " << RESET;
+    // }
+    // cout << endl;
+
+    // //Bottom lane corresponds to Player 2
+    // for (int i = 0; i < 50; i++)
+    // {
+    //     cout << starterMap.getMapPos(1, i) << "2" << " " << RESET;
+    // }
+
+    // cout << RESET << endl;
+    //end of map print
+
+
+    /* CONTINUE REST OF MENU HERE */
+
+
+    return 0;
+
+}
+
